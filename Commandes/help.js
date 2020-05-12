5e5348703fc236909fbba56f22930fe2d0352887
PatrickBerger89 <65043384+PatrickBerger89@users.noreply.github.com>
2020-05-12 13:16:24 +0200

    Corrections ALPHA
    Corrections des fautes d'ortographe

--- Commandes/help.js
+++ Commandes/help.js
@@ -1,91 +1,81 @@
-const Discord = require ("discord.js");
+const Discord = require("discord.js");
 //si la commande help arrive
-module.exports.run = async (client,message, args) =>{
+module.exports.run = async (client, message, args) => {
 	message.author.send({
-		embed:{
+		embed: {
 			color: 3447003,
-			title:`**Commandes fonctionnelles**`,
-			fields: [
-			{
-				name:"+clear [un nombre]",
-				value : "Supprime X message dans le salon textuel en cours, ex: +clear 50"
-			},
-			{
-				name:"+fc [id_ de l'appareil][argent disponible]",
-				value : "Vous retourner les produits les plus rentables à tradder avec les conditions que vous avez indiqué.\n ex +fc 8 5000\n les id sont disponible en tapant la commande +ship_list"
-			},
-			{
-				name:"+help : ",
-				value : "c'est ce que tu lis :)"
-			},
-			{
-				name:"+jeton : ",
-				value : "Vous indique le nombre de jetons restant pour mettre de l'ambiance"
-			},
-			{
-				name:"+kick [utilisateur][raison (optinnelle)]",
-				value : "Déconnecte un utilisateur ex: +kick @un_joueur tu me casses les couilles"
-			},
-			{
-				name:"+kill [utilisateur][raison (optionelle)]",
-				value : "Envoie un utilisateur dans le salon AFK, ex: +kill @un_joueur 'va te calmer 30 secondes'"
-			}
-			,
-			{
-				name:"+play [lien youtube]",
-				value : "Lancera la lecture **audio** d'un lien youtubre, ex : +play https://www.youtube.com/watch?v=vb_k04U3pNs .Attention Missa ne peux être que sur un salon. Si une musique est déjà en cours, celle que vous proposerez sera mis playlist."
-			}
-			,
-			{
-				name:"+ping ",
-				value : "Test son ping (pas sur de la justesse du résultat)"
-			}
-			,
-			{
-				name:"+prison ",
-				value : "Retournera les informations connues sur les prisons"
-			},
-			{
-				name:"+rh [numéro du préset]",
-				value : "Jouera un extrait de film. La liste des extraits est disponible en faisant +sound_list bruitage.., ex: +rh 5"
-			}
-			,
-			
-			{
-				name:"+ship ",
-				value : "Vous donnera la liste des vaisseaux avec leur id unique pris en charge par le site https://mysctools.ovh/"
-			}
-			,
-			{
-				name:"+skip ",
-				value : "Lancera la musique suivante de la playlist"
-			}
-			,
-			{
-				name:"+sound_list",
-				value : "Vous donnera la liste compléte des extraits utilisable avec la commande +rh"
-			}
-			,
-			{
-				name:"+stats ",
-				value : "Connaitre les informations de quelqu'un"
-			}
-			,
-						{
-				name:"+stop ",
-				value : "Coupe la lecture de fichier audio youtube et effacera la playlist"
-			}
-			,
-			
+			title: `**Commandes fonctionnelles**`,
+			fields: [{
+					name: "+clear [un nombre]",
+					value: "Supprime X message dans le salon textuel en cours, ex: +clear 50"
+				},
+				{
+					name: "+fc [id_ de l'appareil][argent disponible]",
+					value: "Vous retourner les produits les plus rentables à tradder avec les conditions que vous avez indiqué.\n ex +fc 8 5000\n les id sont disponibles en tapant la commande +ship_list"
+				},
+				{
+					name: "+help : ",
+					value: "c'est ce que tu lis :)"
+				},
+				{
+					name: "+jeton : ",
+					value: "Vous indique le nombre de jetons restants pour mettre de l'ambiance"
+				},
+				{
+					name: "+kick [utilisateur][raison (optinnelle)]",
+					value: "Déconnecte un utilisateur ex: +kick @un_joueur tu me casses les couilles"
+				},
+				{
+					name: "+kill [utilisateur][raison (optionelle)]",
+					value: "Envoie un utilisateur dans le salon AFK, ex: +kill @un_joueur 'va te calmer 30 secondes'"
+				},
+				{
+					name: "+play [lien youtube]",
+					value: "Lancera la lecture **audio** d'un lien youtubre, ex : +play https://www.youtube.com/watch?v=vb_k04U3pNs .Attention Missa ne peux être que sur un salon. Si une musique est déjà en cours, celle que vous proposerez sera mis playlist."
+				},
+				{
+					name: "+ping ",
+					value: "Test son ping (pas sur de la justesse du résultat)"
+				},
+				{
+					name: "+prison ",
+					value: "Retournera les informations connues sur les prisons"
+				},
+				{
+					name: "+rh [numéro du préset]",
+					value: "Jouera un extrait de film. La liste des extraits est disponible en faisant +sound_list bruitage.., ex: +rh 5"
+				},
+
+				{
+					name: "+ship ",
+					value: "Vous donnera la liste des vaisseaux avec leur id unique pris en charge par le site https://mysctools.ovh/"
+				},
+				{
+					name: "+skip ",
+					value: "Lancera la musique suivante de la playlist"
+				},
+				{
+					name: "+sound_list",
+					value: "Vous donnera la liste complète des extraits utilisables avec la commande +rh"
+				},
+				{
+					name: "+stats ",
+					value: "Connaitre les informations de quelqu'un"
+				},
+				{
+					name: "+stop ",
+					value: "Coupe la lecture de fichier audio youtube et effacera la playlist"
+				},
+
 			],
 			footer: {
-				text:`informations demandé par  l'utilisateur ${message.author.username}`
+				text: `informations demandées par  l'utilisateur ${message.author.username}`
 			}
 		}
 	})
 
 };
 
-module.exports.help ={
+module.exports.help = {
 	name: "help"
 };
--- Events/message.js
+++ Events/message.js
@@ -1,5 +1,5 @@
-const config  =  require ( "./../config.json" );
-const prefix= config.prefix;
+const config = require("./../config.json");
+const prefix = config.prefix;
 
 //chargement de la lib mysql_lite3
 var sqlite3 = require('sqlite3').verbose();
@@ -13,74 +13,67 @@ const monkeys = require("./../class/monkey.js")
 
 
 //si un event client.message arrive
-module.exports = async(client,message)=>{
+module.exports = async (client, message) => {
 
 	//refus des commandes venant d'un autre bot
-	if(message.author.bot) return;
+	if (message.author.bot) return;
 
 	//refus des commandes en MP
-	if(message.channel.type ==="dm") return;
+	if (message.channel.type === "dm") return;
 	//si le message ne commance pas par +
-	if(!message.content.startsWith(prefix)) return;
+	if (!message.content.startsWith(prefix)) return;
 
-	const args = message.content.slice (prefix.length).trim().split(/ +/g);
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
 	const commande = args.shift();
 
 	const cmd = client.commands.get(commande);
-	
+
 	console.log(`L'utilisateur ${monkeys_list[message.author.id].name} à fait appel à la commande : ${commande} , arguments ${args} à ${new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')}`)
 	message.delete();
 
 	//si la commande n'exsite pas
-	if(!cmd) return message.channel.send("Et encore un qui n'est pas capable, de taper une commande valide, ça me fatigue!, Tapes +help pour avoir les commandes actives");;
-	
+	if (!cmd) return message.channel.send("Et encore un qui n'est pas capable de saisir une commande valide, ça me fatigue !, Tapes +help pour avoir les commandes actives");;
+
 	//si la commande concerne la lecture de fichier
-	if(commande==='play' || commande==='skip' || commande==='stop')
-	{
-		
-		if(commande==='play')
-		{
+	if (commande === 'play' || commande === 'skip' || commande === 'stop') {
+
+		if (commande === 'play') {
 			//on retire au membre un jeton de droit à la connerie
 
 			//on prépare l'objet monkeys
-			let monkey= new monkeys();
+			let monkey = new monkeys();
 
 			//on recherche un correspondance avec un utilisateur existant
 			let info = await monkey.search_m(message.author.id).then()
 			//si le membre n'existe pas, on le créé immédiatement
-			if( info === null)
-			{
+			if (info === null) {
 				result = await monkey.create_m(message.author).then()
 				console.log(`création du membre ${message.author['username']}`)
 			}
 
 
 			//creation du tableau de mise à jour
-			let new_data={};
-			new_data.jeton=monkeys_list[message.author.id].jeton--;
+			let new_data = {};
+			new_data.jeton = monkeys_list[message.author.id].jeton--;
 			new_data.time = Date.now();
 			new_data.date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
 
 			//mise à jour des donnée du membres
 			info = await monkey.update_m(new_data).then()
 
-			monkeys_list[message.author.id]=monkey;
+			monkeys_list[message.author.id] = monkey;
 
 			//monkeys_list[message.author.id].jeton--;
 		}
 		serverQueue = queue.get(message.guild.id);
-		if(!args[0] && commande==='play')
-		{
-			return message.channel.send("Vous n'avez spécifiez pas d'adresse youtube à lire");
-		}else
-		{
-			cmd.run(message,serverQueue);
+		if (!args[0] && commande === 'play') {
+			return message.channel.send("Vous n'avez spécifié d'adresse youtube à lire");
+		} else {
+			cmd.run(message, serverQueue);
 		}
 
-	}
-	else
-	{
-		cmd.run(client,message,args);
+	} else {
+		cmd.run(client, message, args);
 	}
 
 
